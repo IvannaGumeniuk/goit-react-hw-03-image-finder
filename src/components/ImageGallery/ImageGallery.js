92723ad75ec713a427737b2d2b1d86fb4c596aba
IvannaGumeniuk <vany93ok@gmail.com>
2022-09-18 00:26:08 +0300

    add ImageGallery

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,18 +1,28 @@
 import { Component } from "react";
 
 export default class ImageGallery extends Component {
-
+    state = {
+        image: null,
+    };
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.searchKey !== this.props.searchKey) {
-            console.log("prev:", prevProps.searchKey);
-            console.log(this.props.searchKey);
+
+            fetch(`https://pixabay.com/api/?q=${this.props.searchKey}&page=1&key=28537959-b0bbf2c2513809284b91fef7c&image_type=photo&orientation=horizontal&per_page=12`)
+                .then(res => res.json())
+                .then(image => this.setState({ image }));
         }
+
     }
+
+    
+
     render() {
         return (
+        <div>
+            {this.state.image &&
             <div>
-                <p>{ this.props.searchKey}</p>
-            </div>
+                {this.state.image.hits.webformatURL}</div>}   
+        </div>
         )
     }
 }
